54a6ca8e24f951ffd2ce6fd1587f547d58698c02
andreapmacedo <aa.andremacedo@gmail.com>
2023-08-09 13:40:50 -0300

    feat(useCallback-model): add model demonstrating the use of useCallback

--- react/useCallback/ts/example.tsx
+++ react/useCallback/ts/example.tsx
@@ -1,20 +1,19 @@
 import { Text, View } from 'react-native';
 
-
-interface ExampleProps {
-  text: string;
-  children?: string; // optional
-}
-
-const Example = ({children, text}: ExampleProps) => {
-  const [name, setName] = useState();
-  const [lastName, setLastName] = useState();
-
-
+const Example = () => {
+  const [name, setName] = useState('name');
+  const [lastName, setLastName] = useState('last name');
   
-  const handleChangeName = () => {
-    setName('new name');
-  };
+  // useCallback is used to prevent the function from being recreated every time the component is rendered.
+  
+  // É imprescindível que o array de dependências seja passado como segundo parâmetro para o useCallback, caso contrário, se o lastName for anteriormente modificado essa mudança não será refletida no callback.
+  // const handleChangeName = useCallback (() => {
+  //   setName(`new name ${lastName}`);
+  // }, []);
+
+  const handleChangeName = useCallback (() => {
+    setName(`new name ${lastName}`);
+  }, [lastName]);
   
   const handleChangeLastName = () => {
     setLastName('new last name');
